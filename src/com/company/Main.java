89de44523617de435accfd00c9e3301626a4badd
AibekArtykbay <201475@astanait.edu.kz>
2021-11-09 20:41:31 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ShapeMaker shapeMaker = new ShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawSquare();
+    }
+}
